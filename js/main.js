8695fdc92bd2f40ddc9a4c4d3acdd5fdb1a75e2b
ezequiel_poeta <ezepoeta46@gmail.com>
2022-07-05 13:10:58 -0300

    changes

--- js/main.js
+++ js/main.js
@@ -58,9 +58,8 @@ while (variable == false) {
     }
 }
 // arrays
-const servicios = [];
-servicios.push('tipo:1', 'Nombre: Lavado Auto', 'Precio: 900', 'Descuento 100' )
-servicios.push('tipo :2', 'Nombre Lavado Camioneta', 'Precio 1000', 'Descuento 150' )
+const servicios = [('tipo 1', 'Nombre: Lavado Auto', 'Precio: 900', 'Descuento 100','tipo 2', 'Nombre Lavado Camioneta', 'Precio 1000', 'Descuento 150' )];
+
 servicios.push('tipo 3', 'Nombre Limpieza Tapizado Basico', 'Precio 3800', 'Descuento 300')
 servicios.push('tipo 4', 'Nombre Limpieza tapizado Completo', 'Precio 5500', 'Descuento 300' )
 
