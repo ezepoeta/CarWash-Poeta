c28ff767de1a71031fbcdf9acdd575513d590b7c
ezequiel_poeta <ezepoeta46@gmail.com>
2022-07-21 19:52:10 -0300

    big changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 // inicio datos para turno
+// let ingreso1 = prompt ('Ingrese su nombre');
+// let ingreso2 = prompt ('Ingrese su numero para contacto');
+// ingreso2 = parseInt(ingreso2);
+// let ingreso3 = prompt('Ingrese su domicilio');
 
-let ingreso1 = prompt ('Ingrese su nombre');
-
-let ingreso2 = prompt ('Ingrese su numero para contacto');
-ingreso2 = parseInt(ingreso2);
-
-let ingreso3 = prompt('Ingrese su domicilio');
-
-if (isNaN (ingreso2)){
-    console.log ('El valor ingresado no corresponde a un numero')
-}
-console.log(ingreso1)
-console.log(ingreso2)
-console.log(ingreso3)
+// if (isNaN (ingreso2)){
+//     console.log ('El valor ingresado no corresponde a un numero')
+// }
+// console.log(ingreso1)
+// console.log(ingreso2)
+// console.log(ingreso3)
 
 
 // costos servicios
@@ -33,29 +30,6 @@ let nuevoPrecio3 = resta(suma(Tapizado, iva(Tapizado)), descuento2)
 
 let variable = false
 
-// while (variable == false) {
-//     let tipo = prompt('Menú Principal. 1.Servicios 2.Productos')
-//     switch (tipo) {
-//         case '1':
-//             alert(servicios.find((el), el.tipo == 1, 2, 3, 4, 5 )),
-//             variable = true
-//             break;
-//         case '2':
-//             alert(productos.find((el), el.tipo == 1, 2, 3, 4, 5, 6 , 7, 8, 9 )),
-//             variable = true
-//             break;
-    
-//         default:
-//             alert('opcion no valida')
-//             break;
-//     }
-    // if (tipo = 1) {
-    //     console.log('El Valor del Lavado seria ' + nuevoPrecio) 
-
-    // } else if (tipo = 2){
-    //     console.log('El valor de la Limpieza de Tapizado es ' + nuevoPrecio3)
-    // }
-// }
 // arrays
 
 const servicios = [
@@ -140,7 +114,7 @@ const productos = [
 console.log(productos)
 
 while (variable == false) {
-    let tipo = prompt('Menú Principal. 1.Servicios 2.Productos')
+    let tipo = console.log('Menú Principal. 1.Servicios 2.Productos');
     switch (tipo) {
         case '1':
             for(const servicio of servicios){
@@ -200,8 +174,9 @@ input1.addEventListener('input',() => {console.log(input3.value)})
 let input4 = document.getElementById("Mensaje");
 input1.addEventListener('input',() => {console.log(input4.value)})
 
+// storage-json
+guardarLocal("Lista de Productos", JSON.stringify(productos))
 
-
-
+const almacenados = JSON.parse(localStorage.getItem ("Lista de Productos"));
 
 
